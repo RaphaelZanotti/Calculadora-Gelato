30834b8073c52bcea35a0778745f6e1d7d4d2f26
Raphael Zanotti <r.zanotti@hotmail.com>
2025-12-22 18:51:22 -0300

    dsadada

--- script.js
+++ script.js
@@ -1,21 +1,15 @@
 import { INGREDIENTES, SABORES } from "./data/ingredientes.js";
 
+console.log("SCRIPT CARREGADO");
 
-
-console.log("SCRIPT OK", INGREDIENTES, SABORES);
-
-
-document.getElementById("btnCalcular").addEventListener("click", () => {
-  console.log("BOTÃO CLICADO");
-});
-
+function calcular() {
+  console.log("CALCULANDO...");
 
   const saborKey = document.getElementById("sabor").value;
   const qtd = Number(document.getElementById("qtdSabor").value);
   const sabor = SABORES[saborKey];
 
   const pesoFinal = (qtd * sabor.solidos) / 0.40;
-
   const leite = pesoFinal * 0.55;
   const creme = pesoFinal * 0.08;
 
@@ -28,3 +22,9 @@ Creme: ${creme.toFixed(1)} g
 
 Peso final: ${(qtd + leite + creme).toFixed(1)} g
 `;
+}
+
+document.getElementById("btnCalcular").addEventListener("click", calcular);
+
+// 👉 se quiser calcular ao carregar
+calcular();
